8ce8d116ceacb16ede4e087ab2a50d176b842e74
Matthew Dibble <mdibble@rogers.com>
2023-01-11 18:47:32 -0500

    To future self: use Node 16.19.0

--- src/components/Thoughts/index.tsx
+++ src/components/Thoughts/index.tsx
@@ -1,30 +1,43 @@
 import { useEffect } from 'react';
 
-import { Title, Break } from '../Common';
+import { Title, Break, Subtitle } from '../Common';
 import Post from './Post';
 import ThoughtsJSON from '../../data/thoughts.json';
+
+const timeThreshold = 1662053511;
 
 function shouldNotifyAboutNewThoughts(): boolean {
   const mostRecentThoughtLS = localStorage.getItem('mostRecentThought');
   const mostRecentThoughtActual = ThoughtsJSON[0].date.toString();
-  if (mostRecentThoughtLS !== mostRecentThoughtActual) {
+  if (mostRecentThoughtLS !== mostRecentThoughtActual
+      && parseInt(mostRecentThoughtActual, 10) >= timeThreshold) {
     return true;
   }
   return false;
 }
 
 function Thoughts(): JSX.Element {
-  const posts = ThoughtsJSON.map((thought) => (
-    <div key={thought.date}>
-      <Post
-        title={thought.title}
-        date={thought.date}
-        icon={thought.icon}
-        writing={thought.writing}
-        match={null}
-      />
-    </div>
+  const posts = ThoughtsJSON.map((thought) => (thought.date > timeThreshold
+    ? (
+      <div key={thought.date}>
+        <Post
+          title={thought.title}
+          date={thought.date}
+          icon={thought.icon}
+          writing={thought.writing}
+          match={null}
+        />
+      </div>
+    )
+    : <></>
   ));
+
+  let postCount = 0;
+  ThoughtsJSON.forEach((thought) => {
+    if (thought.date > timeThreshold) {
+      postCount += 1;
+    }
+  });
 
   useEffect(() => {
     localStorage.setItem('mostRecentThought', ThoughtsJSON[0].date.toString());
@@ -35,7 +48,7 @@ function Thoughts(): JSX.Element {
     <>
       <Title>My Thoughts</Title>
       <Break />
-      {posts}
+      {postCount ? posts : <Subtitle>Nothing right now, check back later!</Subtitle>}
     </>
   );
 }
